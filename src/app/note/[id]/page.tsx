ffea1d2f76ac264c894b3c4dbc402c248b241980
xueguys <yangyuehua_1015@qq.com>
2025-08-19 17:27:42 +0800

    ohiu

--- src/app/note/[id]/page.tsx
+++ src/app/note/[id]/page.tsx
@@ -1,16 +1,11 @@
-import { Note } from '@/types/note';
-import { getNotes } from '@/utils/noteStorage';
 import NoteClient from './NoteClient';
 
 // This function is required for static export
 export async function generateStaticParams() {
   // Since we're using localStorage, we can't generate static params at build time
-  // We'll return an empty array to satisfy the requirement but allow dynamic routing
-  // For a real static export, you would fetch actual data here
-  const notes: Note[] = getNotes();
-  return notes.map(note => ({
-    id: note.id
-  }));
+  // For static export, we return an empty array which will allow any id to be used
+  // The actual note data will be loaded client-side
+  return [];
 }
 
 export default function NotePage({ params }: { params: { id: string } }) {
