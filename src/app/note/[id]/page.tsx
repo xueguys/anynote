0a517fcaac217e5d257f21954036dd6f0e5b1004
xueguys <yangyuehua_1015@qq.com>
2025-08-19 17:47:08 +0800

    tuiytr

--- src/app/note/[id]/page.tsx
+++ src/app/note/[id]/page.tsx
@@ -3,9 +3,9 @@ import NoteClient from './NoteClient';
 // This function is required for static export
 export async function generateStaticParams() {
   // Since we're using localStorage, we can't generate static params at build time
-  // For static export, we return an empty array which will allow any id to be used
+  // We need to return at least one dummy param to satisfy Next.js export requirements
   // The actual note data will be loaded client-side
-  return [];
+  return [{ id: 'dummy' }];
 }
 
 export default async function NotePage({ params }: { params: Promise<{ id: string }> }) {
