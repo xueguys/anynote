2c8a55ed24979ac14eea246557a69b5ba3fde5c4
xueguys <yangyuehua_1015@qq.com>
2025-08-19 17:16:58 +0800

    uggg

--- src/app/note/[id]/page.tsx
+++ src/app/note/[id]/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { Box, Paper, TextField, ToggleButton, ToggleButtonGroup, Tooltip } from '@mui/material';
